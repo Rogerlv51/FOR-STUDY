9ee04ef744287793661e7a97baa746ef6c52aa57
Rogerlv51 <949608370@qq.com>
2023-03-04 15:40:40 +0800

    scrapy

--- scrapy.py
+++ scrapy.py
@@ -1,26 +1,22 @@
+# 不开启浏览器得到返回值
+    # -*- coding:utf-8 -*-
 from selenium import webdriver
-
-# 创建 Chrome 浏览器实例
-driver = webdriver.Chrome()
-
-# 加载百度搜索结果页面
-driver.get('https://www.baidu.com/s?wd=selenium')
-
-# 获取所有搜索结果的标题和链接
-results = driver.find_elements_by_xpath('//div[@class="result c-container "]')
-
-for result in results:
-    # 获取结果标题
-    title_element = result.find_element_by_xpath('.//h3/a')
-    title = title_element.text
-
-    # 获取结果链接
-    link_element = result.find_element_by_xpath('.//h3/a')
-    link = link_element.get_attribute('href')
-
-    # 打印结果标题和链接
-    print(title)
-    print(link)
-
-# 关闭浏览器实例
+import time
+ 
+ 
+# 加载启动项，这里设置headless，表示不启动浏览器，只开一个监听接口获取返回值
+option = webdriver.ChromeOptions()
+option.add_argument('headless')
+ 
+# 通过url访问浏览器
+driver = webdriver.Chrome(chrome_options=option)
+url = 'https://www.baidu.com'
+ 
+driver.get(url=url)
+ 
+# 这个是打印page，有的全部打下来
+print(driver.page_source)
+time.sleep(2)
+ 
+# 退出并关闭浏览器
 driver.quit()
